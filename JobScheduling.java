1a1076dc66bad6a8cf0461d487d1428835f2d24c
zozeyx <126737048+zozeyx@users.noreply.github.com>
2024-12-03 16:06:36 +0900

    Update JobScheduling.java

--- JobScheduling.java
+++ JobScheduling.java
@@ -32,29 +32,29 @@ public class JobScheduling {
             machines.get(minMachine).add("t" + (i + 1) + "(" + startTime + "시)");  // 작업 이름(t1, t2, ...)과 시작 시간을 추가
         }
 
-        // 시간대 0부터 12까지 표시
-        System.out.println("시간대 0  1  2  3  4  5  6  7  8  9  10  11  12");
+        // m/t 출력
+        System.out.print("m/t ");
+        for (int i = 0; i < 11; i++) {
+            System.out.print(i + "  ");  // 시간대 출력 (0부터 10까지)
+        }
+        System.out.println();
 
-        // 각 기계별 작업 배치 결과 출력
+        // 각 기계별 작업 배치 결과 출력 (m4, m3, m2, m1 순으로 출력)
         for (int i = m - 1; i >= 0; i--) {  // m4, m3, m2, m1 순으로 출력
             System.out.print("m" + (i + 1) + " ");
-            int[] timeSlot = new int[13]; // 0부터 12까지 시간 슬롯 (13개)
-            Arrays.fill(timeSlot, -1); // 초기값 -1로 설정 (작업이 없음을 표시)
+            String[] timeSlot = new String[11]; // 0부터 10까지 시간 슬롯 (11개)
+            Arrays.fill(timeSlot, "    "); // 초기값 공백으로 설정 (작업이 없음을 표시)
 
             // 기계에서 실행된 각 작업의 시작 시간을 시간 슬롯에 배치
             for (String task : machines.get(i)) {
                 int startTime = Integer.parseInt(task.substring(task.indexOf('(') + 1, task.indexOf('시')));
                 int taskId = Integer.parseInt(task.substring(1, task.indexOf('('))) - 1;  // 작업 번호(t1, t2, t3 ...) 추출
-                timeSlot[startTime] = taskId + 1;  // 작업 번호를 해당 시작 시간에 배치
+                timeSlot[startTime] = "t" + (taskId + 1);  // 작업 번호를 해당 시작 시간에 배치
             }
 
-            // 시간 슬롯을 출력
-            for (int time = 0; time < 13; time++) {
-                if (timeSlot[time] != -1) {
-                    System.out.print("t" + timeSlot[time] + " ");
-                } else {
-                    System.out.print("    ");  // 해당 시간대에 작업이 없으면 공백 출력
-                }
+            // 각 시간대에 배치된 작업을 출력
+            for (int time = 0; time < 11; time++) {
+                System.out.print(timeSlot[time] + "  ");  // 각 시간대에 배치된 작업을 출력
             }
             System.out.println();
         }
